5a750af3daa84532c0fb4ec5f867eb9906595d4c
sol.lee <sol.lee.0929@gmail.com>
2021-01-09 14:32:21 -0800

    main tabs set up

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -2,14 +2,19 @@ import React, { useEffect, useState } from "react";
 import { Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { Route, Link } from "react-router-dom";
+import MainTabs from "./MainTabs";
 
-const useStyles = makeStyles((theme) => ({
-  landingContainer: {
-    margin: theme.spacing.unit * 2,
-  },
-}));
+// const useStyles = makeStyles((theme) => ({
+//   landingContainer: {
+//     margin: theme.spacing.unit * 2,
+//   },
+// }));
 
 export default function Landing() {
-  const classes = useStyles();
-  return <div className={classes.landingContainer}>testtest</div>;
+  // const classes = useStyles();
+  return (
+    <div>
+      <MainTabs />
+    </div>
+  );
 }
