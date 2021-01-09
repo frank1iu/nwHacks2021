e9180d9c4a0c253204ddda8d5ac9709e150e107b
Sina Personal <personal@MacBook-Pro.local>
2021-01-09 13:17:07 -0800

    init

--- server/app.js
+++ server/app.js
@@ -5,7 +5,6 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
-const pingRouter = require("./routes/ping");
 
 const { json, urlencoded } = express;
 
@@ -18,15 +17,14 @@ app.use(cookieParser());
 app.use(express.static(join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/ping", pingRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/welcome", function (req, res, next) {
-  res.status(200).send({ welcomeMessage: "Step 1 (completed)" });
+  res.status(200).send({ welcomeMessage: "welcome" });
 });
 
 module.exports = router;
