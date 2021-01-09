cf62a35d92b2102561dc100cb0c381db940c18c6
frank1iu <frankliupiye@icloud.com>
2021-01-09 15:11:57 -0800

    Implement health check

--- server/routes_src/index.ts
+++ server/routes_src/index.ts
@@ -3,8 +3,8 @@ import FirestoreAccess from "./Helpers/Firestore";
 const router = express.Router();
 const db = new FirestoreAccess(process.env.FIRESTORE_PROJECT, process.env.FIRESTORE_KEYFILE);
 
-router.get("/welcome", function (req, res, next) {
-  res.status(200).send({ welcomeMessage: "welcome" });
+router.get("/health", function (req, res, next) {
+  res.status(200).send({ up: true });
 });
 
 router.post("/register", async (req, res, next) => {
