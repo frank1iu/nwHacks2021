fb6ea5fc1199fe979e9efd54ac9a3dd572d6a999
frank1iu <frankliupiye@icloud.com>
2021-01-09 13:28:20 -0800

    Setup TypeScript workspace for server

--- server/routes_src/index.ts
+++ server/routes_src/index.ts
@@ -1,8 +1,8 @@
-const express = require("express");
+import express = require("express");
 const router = express.Router();
 
 router.get("/welcome", function (req, res, next) {
   res.status(200).send({ welcomeMessage: "welcome" });
 });
 
-module.exports = router;
+export default router;
