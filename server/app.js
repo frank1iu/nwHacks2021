3867283aa0be243626cb0cbfc1eac1a3c2e80b3c
frank1iu <frankliupiye@icloud.com>
2021-01-09 20:25:04 -0800

    Make express server serve static content

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ app.use(logger("dev"));
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(join(__dirname, "public")));
+app.use(express.static(join(__dirname, "static")));
 
 app.use("/", indexRouter);
 
