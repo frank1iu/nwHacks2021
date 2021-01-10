274788aec115b9b3b801059cc36534fc4b5b9448
sol.lee <sol.lee.0929@gmail.com>
2021-01-09 17:25:03 -0800

    search bars

--- client/src/components/Items.js
+++ client/src/components/Items.js
@@ -1,8 +1,10 @@
 import React from "react";
+import placeholder from "./placeholder.png";
 
 export default function Items() {
   return (
     <div>
+      <img alt="" src={placeholder} />
       <p>Organization</p>
       <p>Offering</p>
       <p>etc.</p>
