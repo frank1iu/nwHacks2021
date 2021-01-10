c85d0bd86ec975d0a5943bb215213e0ee5cce360
sol.lee <sol.lee.0929@gmail.com>
2021-01-09 19:43:04 -0800

    buttons and colors

--- client/src/pages/TopBar.js
+++ client/src/pages/TopBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
 import { Divider, Grid, Paper } from '@material-ui/core';
 import classes from "./pages.css"
 import {
@@ -18,11 +19,14 @@ import {
 } from "@material-ui/icons";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(5),
-    },
+  offerButton: {
+    color: "white",
+    background: '#8FDDEC'
   },
+  requestButton: {
+    color: "white",
+    background: '#FFB478'
+  }
 }));
 
 export default function TopBar() {
@@ -31,14 +35,27 @@ export default function TopBar() {
   return (
     <div>
         <img alt="share street logo" style={{ width: 120, height: 45 }} src={logo}/>
-        <InboxOutlined />
-        <MessageOutlined />
-        <NotificationsOutlined />
+        <IconButton>
+          <InboxOutlined/>
+        </IconButton>
+        <IconButton>
+          <MessageOutlined />
+        </IconButton>
+        <IconButton>
+         <NotificationsOutlined />
+        </IconButton>
+      
         <NavLink to="/offerform">
-          <Button variant="contained" color={"8FDDEC"}>Create Offer</Button>
+          <Button 
+            className={classes.offerButton}
+            variant="contained" 
+          >Create Offer</Button>
         </NavLink>
         <NavLink to="/requestform">
-          <Button variant="contained">Create Request</Button>
+          <Button 
+            className={classes.requestButton}
+            variant="contained"
+          >Create Request</Button>
         </NavLink>
     </div>
   );
