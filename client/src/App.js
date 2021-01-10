f7427266a67bae703d92d5245552c96de1a26beb
sol.lee <sol.lee.0929@gmail.com>
2021-01-09 16:43:39 -0800

    some links, additional tabs

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,29 @@
 import React from "react";
 import { MuiThemeProvider } from "@material-ui/core";
-import { BrowserRouter, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
 import { theme } from "./themes/theme";
 import LandingPage from "./pages/Landing";
-
+import CreateNewRequest from "./pages/CreateNewRequest";
+import CreateNewOffer from "./pages/CreateNewOffer";
 import "./App.css";
 
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Route path="/" component={LandingPage} />
-      </BrowserRouter>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/requestform" component={CreateNewRequest} />
+          <Route path="/offerform" component={CreateNewOffer} />
+        </Switch>
+      </Router>
     </MuiThemeProvider>
   );
 }
---
+++ client/src/components/Items.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Items() {
+  return (
+    <div>
+      <p>Organization</p>
+      <p>Offering</p>
+      <p>etc.</p>
+    </div>
+  );
+}
